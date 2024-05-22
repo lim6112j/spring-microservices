1ba5fd0cfb5d89ec5505c2015a746a1c26faa697
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2024-05-22 21:33:26 +0900

    path rewrite

--- api-gateway/src/main/java/com/ciel/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/ciel/microservices/apigateway/ApiGatewayConfiguration.java
@@ -12,18 +12,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfiguration {
-		@Bean
-		public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-				Function<PredicateSpec, Buildable<Route>> routeFunction =
-						p -> p.path("/get")
-						.filters(f -> f.addRequestHeader("MyHeader", "hello world")
-										 .addRequestParameter("my param", "10"))
-						.uri("http://httpbin.org:80");
-				return builder.routes()
-						.route(routeFunction)
-						.route(p->p.path("/dispatch-service/**").uri("lb://dispatch-service"))
-						.route(p->p.path("/location-info/**").uri("lb://location-info"))
-						.route(p->p.path("/location-info-feign/**").uri("lb://location-info"))						
-						.build();
-		}
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		Function<PredicateSpec, Buildable<Route>> routeFunction = p -> p.path("/get")
+				.filters(f -> f.addRequestHeader("MyHeader", "hello world")
+						.addRequestParameter("my param", "10"))
+				.uri("http://httpbin.org:80");
+		return builder.routes()
+				.route(routeFunction)
+				.route(p -> p.path("/dispatch-service/**").uri("lb://dispatch-service"))
+				.route(p -> p.path("/location-info/**").uri("lb://location-info"))
+				.route(p -> p.path("/location-info-feign/**").uri("lb://location-info"))
+				.route(p -> p.path("/location-info-new/**")
+						.filters(f -> f.rewritePath("/location-info-new/(?<segment>.*)", "/location-info-feign/${segment}"))
+						.uri("lb://location-info"))
+				.build();
+	}
 }
