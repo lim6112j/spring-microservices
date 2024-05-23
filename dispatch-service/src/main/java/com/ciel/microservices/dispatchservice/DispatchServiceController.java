577901d8b11e31cf556961da2ccad888a83ccef7
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2024-05-23 22:25:41 +0900

    ...

--- dispatch-service/src/main/java/com/ciel/microservices/dispatchservice/DispatchServiceController.java
+++ dispatch-service/src/main/java/com/ciel/microservices/dispatchservice/DispatchServiceController.java
@@ -1,5 +1,7 @@
 package com.ciel.microservices.dispatchservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DispatchServiceController {
+		private Logger logger = LoggerFactory.getLogger(DispatchServiceController.class);
 		@Autowired
 		private GeocodeRepository repo;
 		@Autowired
 		private Environment environment;
 		@GetMapping("/dispatch-service/from/{from}/to/{to}")
 		public Geocode retrieveGeocode(@PathVariable String from, @PathVariable String to) {
+				logger.info("retrieveGeocode called with {} to {}", from, to);
 				//				Geocode geocode = new Geocode(1000L, from, to);
 				String port = environment.getProperty("local.server.port");
 				Geocode geocode = repo.findByFromAndTo(from, to);
