3e7d95e90f77cd95711a37cf84b5c4790fc00223
ben lim <lim6112j@gmail.com>
2024-07-04 10:23:29 +0900

    eureka down signal sending when C-c

--- node-server/index.js
+++ node-server/index.js
@@ -1,41 +1,54 @@
-const express = require('express')
-const Eureka = require('eureka-js-client').Eureka
+const express = require("express");
+const Eureka = require("eureka-js-client").Eureka;
 // const eureka_client = new Eureka({
 // 		cwd: `${__dirname}/config`,
 // 		filename: 'eureka-config'
 // })
 const eureka_client = new Eureka({
-		instance: {
-        app: 'nodejs-service',
-        hostName: 'localhost',
-        ipAddr: '127.0.0.1',
+    instance: {
+        app: "nodejs-service",
+        hostName: "localhost",
+        ipAddr: "127.0.0.1",
         port: {
-            '$': 8300,
-            '@enabled': 'true',
+            $: 8300,
+            "@enabled": "true",
         },
-        vipAddress: 'nodejs-service',
+        vipAddress: "nodejs-service",
         dataCenterInfo: {
-            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-            name: 'MyOwn',
+            "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+            name: "MyOwn",
         },
     },
     eureka: {
-        host: 'localhost',
+        host: "localhost",
         port: 8761,
-        servicePath: '/eureka/apps'
-    }
-})
+        servicePath: "/eureka/apps",
+    },
+});
 
-const app = express()
-const port = 8300
+const app = express();
+const port = 8300;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-eureka_client.start(error => {
-    console.log('----------------------------\nEureka client started with error:', JSON.stringify(error));
+    console.log(`Example app listening on port ${port}`);
+});
+eureka_client.start((error) => {
+    console.log(
+        "----------------------------\nEureka client started with error:",
+        JSON.stringify(error),
+    );
+});
+process.on("SIGINT", function() {
+    // C-c event handling for client down to Eureka server
+    eureka_client.stop((error) => {
+        console.log(
+            `----------------------\nEureka client stopped with error: `,
+            JSON.stringify(error),
+        );
+        process.exit();
+    });
 });
